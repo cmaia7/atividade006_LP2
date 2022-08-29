c6629e089d11ac7d941d22dd20ca6cb9c640e233
cassiel-st4vr0s <csdmd.snf21@uea.edu.br>
2022-08-29 13:45:08 -0400

    Envio da questão 5 da lista 1

---
+++ quest5_l1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc,char **argv)
+{
+
+        int a,b,c;
+
+        printf("Preço do produto 'a' em reais:\n");
+        scanf("%d",&a);
+
+        printf("Preço do produto 'b' em reais:\n");
+        scanf("%d",&b);
+
+        printf("Preço do produto 'c' em reais:\n");
+        scanf("%d,",&c);
+
+        if (a<b&&a<c)
+        {
+                printf("Compre o produto 'a'\n");
+        }
+
+        if (b<a&&b<c)
+        {
+                printf("Compre o produto 'b'\n");
+        }
+
+        return 0;
+}

f616339291b64cf9193806620c0ce61cdc6ae8d7
cassiel-st4vr0s <csdmd.snf21@uea.edu.br>
2022-08-29 13:37:31 -0400

    Envio da questão 2 da lista 1

---
+++ quest2_l1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc,char **argv)
+{
+        int a;
+
+        printf("Digite um valor:\n");
+        scanf("%d",&a);
+
+        if (a>=0);
+        {
+                printf("O valor digitado é positivo\n");
+        }
+         else
+        {
+                printf("O valor digitado é negativo\n");
+        }
+
+        return 0;
+}

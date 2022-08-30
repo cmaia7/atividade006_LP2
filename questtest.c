0206654e08c5b89d9977bd7c5b62f96a890b38a0
cassiel-st4vr0s <csdmd.snf21@uea.edu.br>
2022-08-30 00:01:03 -0400

    envio questão 9 da lista 1

---
+++ questtest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc,char** argv)
+{
+	printf("hellor world");
+
+	return 0;
+}
---
+++ teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc,char** argv)
+{
+        printf("hellor world");
+
+        return 0;
+}
+
